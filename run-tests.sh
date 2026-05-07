10eb02b20d8f5c5a3faab88d82631f3453acc0d1
aicoder <aicoder@local>
2026-05-06 21:20:18 -0300

    Improve test script UX: default yes, shorter timeout, timeout triggers run

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,8 @@
 python -m pytest tests/ -v --tb=short --ignore=tests/integration
 
 echo ""
-read -t 60 -p "Run integration tests? [y/N]: "
-if [[ "$REPLY" =~ [yY] ]]; then
+read -t 15 -p "Run integration tests? [Y/n]: "
+ret=$?
+if (( ret == 142 )) || [[ ! "$REPLY" =~ [nN] ]]; then
     python -m pytest tests/integration
 fi
